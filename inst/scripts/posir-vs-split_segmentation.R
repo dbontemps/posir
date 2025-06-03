8502bfad5f5f00e02a1151ff79579ad4face0d5b
Pierre Neuvial <pierre.neuvial@math.cnrs.fr>
2025-06-03 11:57:46 +0200

    move to specific script

---
+++ inst/scripts/posir-vs-split_segmentation.R
@@ -0,0 +1,27 @@
+# Segmentation: Comparision between POSIR and basic data splitting data
+# splitting:
+# - observations with even index are used to obtain segmentation
+# - observations with odd index are use to build confidence intervals
+# see POSIR paper for additional details.
+
+
+seq_k <- c(5, 10, 20, 50)
+
+config_list <- lapply(seq_k, function(k) {
+  cbind(k, c = seq(from = k*.005, to = 1, length.out = 101))
+})
+configs <- as.data.frame(Reduce(rbind, config_list))
+res <- dplyr::mutate(configs, ratio = CI_width_ratio(c,k))
+res$L <- as.factor(res$k)
+
+library("ggplot2")
+p <- ggplot(res, aes(x = c, y = ratio, colour = L, group = L)) +
+  geom_line() +
+  geom_line(y = 1, linetype = "dashed") +
+  theme(legend.position = c(0.9, 0.3)) +
+  labs(title = expression(paste("Quantile ratios for ", delta, " = c/L"))) +
+  guides(color = guide_legend(reverse = TRUE))
+p
+
+ggsave(p, file = "posir-vs-split_segmentation.pdf",
+       width = 6, height = 4)
